36242a5237d80d0c063104cd85bd1071c21e25db
Shiva1231234 <109754082+Shiva1231234@users.noreply.github.com>
2022-12-16 04:39:03 -0800

    Add files via upload

---
+++ introductionof.cpp
@@ -0,0 +1,152 @@
+assignment 23
+question =1
+/*
+#include <iostream>
+using namespace std;
+int main() 
+{
+   cout << "my sirg";
+    cout<<endl; 
+   
+}
+*\
+Question =2
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hello";
+    cout<<endl;
+    cout<<"mysirg";
+}
+*\
+Question =3
+/*
+#include<iostream>
+using namespace std;
+int main()
+{ 
+int a,b,y;
+    cout<<"enter two numbers";
+    cin>>a >> b;
+    y=a+b;
+    
+    cout<<"sum of two numbers"<<y;
+    cout<<endl;
+}
+*\
+Question =4
+/*
+#include<iostream>
+using namespace std;
+int main()
+{ 
+    int a,r;
+    cout<<"enter the radius of the circle";
+    cin>>r;
+    a=2.54*r*r;
+    cout<<"area of the circle"<<  a; 
+    cout<<endl;
+}
+*\
+Question =5
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    int v,l,w,h;
+    cout<<"enter the three element";
+    cin>>l>>w>>h;
+    v=l*w*h;
+    cout<<"volume of the cuboid"<<v;
+}
+*\
+Question =6
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e,f,g,h,average,sum=0;
+    cout<<"enter the three numbrs";
+    cin>>a>>b>>c>>d>>e>>f>>g>>h;
+    sum=a+b+c+d+e+f+g+h;
+    average=sum/8;
+    cout<<"average of three numbers"<<average;
+    cout<<endl;
+}
+*\
+Question =7
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r,v;
+    cout<<"enter the number";
+    cin>>r;
+    v=r*r;
+    cout<<"square of the number"<<v;
+    cout<<endl;
+}
+*\
+Question =8
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=40,b=60;
+    cout<<"swap two numbers\n";
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    cout<<"first number a=" <<a;
+    cout<<endl;
+    cout<<"second number b=" <<b; 
+}
+*\
+Question =9
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    int max;
+    int a,b;
+    cout<<"enter two numbers";
+    cin>>a>>b;
+    if(a<b)
+    {
+        cout<<"number is minimum a= "<<a;
+    }
+    if(b>a)
+    {
+        cout<<"number is maximumb= "<<b;
+    }
+}
+*\
+Question =10
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[10],i,sum=0;
+    cout<<"enter the ten numbers";
+    for(i=0;i<10;i++)
+    {
+    cin>>a[i];
+    }
+    for(i=0;i<10;i++)
+    {
+        sum=sum+a[i];
+    }
+    cout<<"sum of the teen numbers"<<sum;
+    cout<<endl;
+
+}
+*\
+
